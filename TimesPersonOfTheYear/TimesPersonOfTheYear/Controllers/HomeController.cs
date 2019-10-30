dfeef40da6a89c2dd4936c07e0c27763e6aa4dd5
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-29 23:55:00 -0700

    scaffolding, im working on constructor function, following amandas walkthrough from today.

---
+++ TimesPersonOfTheYear/TimesPersonOfTheYear/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimesPersonOfTheYear.Models;
+
+namespace TimesPersonOfTheYear.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var random = TimePerson.GetPersons(1950, 1960);
+            return View();
+        }
+    }
+}
